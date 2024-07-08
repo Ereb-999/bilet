c988370610754df0f2661f913c24b24567608191
Ereb-999 <Naofumi999@yandex.ru>
2024-07-08 17:53:19 +0400

    chapter final

--- src/main/java/ru/hogwarts/Main.java
+++ src/main/java/ru/hogwarts/Main.java
@@ -1,6 +1,16 @@
 package ru.hogwarts;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args){
+        List<String> names = Arrays.asList("Roma", "Peter", "Masha");
 
+        List<String> listName = names.stream()
+                .map(name -> name.substring(1))
+                .sorted()
+                .collect(Collectors.toList());
+                System.out.println(listName);
     }
 }
